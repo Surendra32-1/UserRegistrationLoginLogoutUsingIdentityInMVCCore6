c10ac37ab29c9108b1e0e26049e43bc7d10eb8e7
surendra <surendra@riddhasoft.com>
2023-06-26 22:52:41 +0545

    IdentityLoginLogoutRegistration

---
+++ LoginLogoutUsingIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginLogoutUsingIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
